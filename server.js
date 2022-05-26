1be3bb830ef884c86e6eea26f26b861bed45b904
owin66 <78900270+owin66@users.noreply.github.com>
2022-05-26 22:11:25 +0300

    res_send

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3000;
 
-server.listen(PORT, 'localhost', (error) => {
+app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>')
+})
