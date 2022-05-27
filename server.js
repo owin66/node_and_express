4a92e171b23b1ebb1d6b3040bf95c42d65a51b6b
owin66 <78900270+owin66@users.noreply.github.com>
2022-05-27 16:20:50 +0300

    redirect

--- server.js
+++ server.js
@@ -7,18 +7,25 @@ const PORT = 3000;
 
 const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`)
 
+//функция перехода путей
+
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(createPath('index'))
+    res.sendFile(createPath('index'))//путь
 })
+
 app.get('/contacts', (req, res) => {
-    res.sendFile(createPath('contacts'))
+    res.sendFile(createPath('contacts')) //путь
+})
+
+app.get('/about-us', (req, res) => {
+    res.redirect('/contacts') // перенаправление
 })
 app.use((req,res)=>{
     res
-        .status(404)
-        .sendFile(createPath('error'))
+        .status(404) //ошибка
+        .sendFile(createPath('error'))//перенаправление на ошибку
 })
